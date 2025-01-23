af0c6cc8db4f3b77e104e9cb24788cf02be2c5ef
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2025-01-23 15:14:23 +0100

    add migration and update Db

---
+++ user-city-management-service/Migrations/20250123173335_InitialDb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace user_city_management_service.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false),
+                    IsBanned = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "IsBanned", "PasswordHash", "Role", "Username" },
+                values: new object[] { new Guid("e79dd6c6-c686-40a3-a0da-7dac043ae9a3"), "admin@example.com", false, "$2a$11$x9P58Wt9r3HTMIf.qpuBT.Q5B1YsrYgcjMboccRCDReACbvY8ydJ2", 0, "FullAdmin" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
