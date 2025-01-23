a3c78aaf0eaa5f9172d7d02ac134a09449828f12
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2025-01-23 10:56:07 +0100

    add Dtos Models

---
+++ user-city-management-service/Application/Dtos/AllUsersDto.cs
@@ -0,0 +1,5 @@
+﻿namespace user_city_management_service.Application.Dtos
+{
+    public record AllUsersDto(string Username, string Role,bool IsBanned);
+
+}
---
+++ user-city-management-service/Application/Dtos/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using user_city_management_service.Domain.Enums;
+
+namespace user_city_management_service.Application.Dtos;
+
+public record UserDto(string Username, string Email, string Role);
+public record RegisterRequest(
+    [Required] string Username,
+    [Required, EmailAddress] string Email,
+    [Required, MinLength(6)] string Password,
+    [Required] Role Role 
+); 
+public record LoginRequest(string Username, string Password);
